2b96500af6798d681b10ff71c761c0f8c1d1c56a
DiegoAlvesOL <dgo.oliveira@gmail.com>
2025-05-19 23:49:26 +0100

    feat(models): create PasswordEntry class to represent saved password data

--- models/password_entry.py
+++ models/password_entry.py
@@ -1,12 +1,22 @@
+from db.database import insert_password
+
+
 class passwordEntry:
-    def __init__(self, item_name, user_entry, website_address, password):
-        self.item_name = item_name
-        self.user_entry = user_entry
-        self.website_address = website_address
+    def __init__(self, item,user,site,password):
+        self.item = item
+        self.user = user
+        self.site = site
         self.password = password
 
+
+    def save_to_db(self):
+        insert_password(self.item,
+                        self.user,
+                        self.site,
+                        self.password)
+
     def __str__(self):
-        return f"{self.item_name} | {self.user_entry} |{self.website_address} |{self.password}"
+        return f"{self.item} | {self.user} |{self.site} |{self.password}"
 
 entry = passwordEntry
 print(entry)
