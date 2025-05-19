7df807dbaa26ac20e64c487c615df15cb75e8b18
DiegoAlvesOL <dgo.oliveira@gmail.com>
2025-05-19 15:56:30 +0100

    refactor: update generate_password function to apply new security rules

--- passwordFunctions/passwordGenerator.py
+++ passwordFunctions/passwordGenerator.py
@@ -3,13 +3,22 @@ import string
 
 # length = int(input("Enter your password Length: "))
 
-def generate_password (length):
+def generate_password (length, use_digits=True, use_letter=True, use_symbols=True):
     characters = ""
 
-    characters = string.ascii_letters + string.digits + string.punctuation
-    password = ""
 
-    for i in range (length):
+    if use_digits:
+        characters = characters + string.digits
+    if use_letter:
+        characters = characters + string.ascii_letters
+    if use_symbols:
+        characters = characters + string.punctuation
+
+    if not characters:
+        return "⚠️Error: No character types selected."
+
+    password = ""
+    for i in range(length):
         password = password + random.choice(characters)
     return password
 
